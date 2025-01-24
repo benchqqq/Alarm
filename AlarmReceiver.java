676394ce992ac1dfd304e4508eaeb8d1b0669caf
benchqqq <71522221+benchqqq@users.noreply.github.com>
2025-01-24 21:01:59 +0100

    Update AlarmReceiver.java

--- AlarmReceiver.java
+++ AlarmReceiver.java
@@ -1,1 +1,21 @@
-. 
+package com.example.alarmapp;
+
+  import android.content.BroadcastReceiver;
+  import android.content.Context;
+  import android.content.Intent;
+  import android.media.Ringtone;
+  import android.media.RingtoneManager;
+  import android.net.Uri;
+
+  public class AlarmReceiver extends BroadcastReceiver {
+      @Override
+      public void onReceive(Context context, Intent intent) {
+          // تشغيل صوت التنبيه
+          Uri alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+          if (alarmUri == null) {
+              alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+          }
+          Ringtone ringtone = RingtoneManager.getRingtone(context, alarmUri);
+          ringtone.play();
+      }
+  }
